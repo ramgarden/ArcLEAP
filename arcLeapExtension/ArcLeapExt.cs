4176996a31766c08e19e1096c59ff386a5a4fe87
Josh Pritt <ramgarden@gmail.com>
2013-07-15 13:28:50 -0500

    Added solution and initial structure
    Added the C# solution and project with the class that will implement the
    ArcMap Extension that will do the listening for the LEAP controller.

---
+++ arcLeapExtension/ArcLeapExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArcLEAP
+{
+    /// <summary>
+    /// ESRI ArcGIS ArcMap extension that will listen to the LEAP
+    /// motion controller when loaded.  It will then interpret 
+    /// hand and finger gestures as standard ArcMap controls like
+    /// zoom, pan, and identify, etc.
+    /// </summary>
+    public class ArcLeapExt
+    {
+
+    }
+}
